a60b1108bc54edcfe71c17c671cc43b7e8a38c9d
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2024-08-14 11:38:56 +0000

    [initial] fix: front style code

---
+++ src/front/js/pages/Error404.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export const Error404 = () => {
+
+  return (
+    <h1 className="text-center text-danger mt-auto">
+      Error 404 - Page not found
+    </h1>
+  )
+}
