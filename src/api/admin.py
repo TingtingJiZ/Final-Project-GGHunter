f16afe2a03900c3e3265c11d75af4311327cfc0c
Tingting Ji Zhou <168580008+TingtingJiZ@users.noreply.github.com>
2024-08-19 20:19:16 +0200

    Revert "Gender"

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,18 +1,13 @@
 import os
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from .models import db, Users, Games, GamesCharacteristic, Plataforms, Genders, GameGenders
+from .models import db, Users
 
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    app.config['FLASK_ADMIN_SWATCH'] = 'darkly'
+    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(Users, db.session))  # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
-    admin.add_view(ModelView(Games, db.session))
-    admin.add_view(ModelView(GamesCharacteristic, db.session))
-    admin.add_view(ModelView(Plataforms, db.session))
-    admin.add_view(ModelView(Genders, db.session))
-    admin.add_view(ModelView(GameGenders, db.session))
