8985c6b3a1bf163ec4663e1b1c9cb22c809e3bbf
Francisco Javier Carrillo Carrillo <133151654+FcoJCarrillo@users.noreply.github.com>
2024-08-22 17:54:46 +0000

    [endpoint]: feat:JWT

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from api.models import db, Users
-
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 
 api = Blueprint('api', __name__)
 CORS(api)  # Allow CORS requests to this API
