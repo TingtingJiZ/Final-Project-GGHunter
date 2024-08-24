9e337ddea4f1b8bd18c2781f40bd7cbd8bc8bf0b
Francisco Javier Carrillo Carrillo <133151654+FcoJCarrillo@users.noreply.github.com>
2024-08-24 09:12:34 +0000

    [endpoint]wip:login/signup and user

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users
+from api.models import db, Users, Favorites, SocialAccounts, Comments, Media, Games, GameCharacteristics, Platforms, Stores, Genders, GameGenders
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt_identity
@@ -18,3 +18,56 @@ def handle_hello():
     response_body = {}
     response_body['message'] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     return response_body, 200
+
+
+@api.route('/signup', methods=['POST'])
+def handle_singup():
+    response_body = {}
+    data = request.json
+    email = data.get("email", None).lower()
+    password = data.get("password", None)
+    existing_user = db.session.execute(db.select(Users).where(Users.email == email)).scalar()
+    
+    if existing_user:
+        return jsonify({"message": "El usuario con este correo ya existe."}), 409
+    
+    new_user = Users(
+        email = email,
+        password = data.get("password"),
+        is_active = True,
+        rol = 'user'
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    user = db.session.execute(db.select(Users).where(Users.email == email)).scalar()
+    access_token = create_access_token(identity={'email': user.email,
+                                                'user_id': user.id,
+                                                'rol': user.rol})
+    response_body['results'] = user.serialize()
+    response_body['message'] = 'User registrado y logeado'
+    response_body['access_token'] = access_token
+    return response_body, 201
+
+
+@api.route('/login', methods=['GET'])
+def handle_users():
+    response_body = {}
+    data = request.json
+    email = data.get("email", None).lower()
+    password = data.get("password", None)
+    user = db.session.execute(db.select(Users).where(Users.email == email, Users.password == password, Users.is_active == True)).scalar()
+    
+    if not user:
+        response_body["message"] =f"El usuario {email} es desconocido para el sistema"
+        return response_body, 404
+    
+    if not user.is_active:
+        response_body["message"] =f"El usuario {email} no esta activo en la página web"
+        return response_body, 404
+    
+    access_token = create_access_token(identity={'email':email, 'user_id':user.id, 'is_rol':user.rol})
+    response_body['results'] = user.serialize()
+    response_body['message'] = 'User logged'
+    response_body['message'] = access_token
+    return response_body, 201
+    
