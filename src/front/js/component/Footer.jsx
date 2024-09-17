df09b32d816a8c6f62d97df7b22b65282fc991f3
Tingting Ji Zhou <168580008+TingtingJiZ@users.noreply.github.com>
2024-09-17 16:47:50 +0000

    [front] responsive working in footer and navbar

--- src/front/js/component/Footer.jsx
+++ src/front/js/component/Footer.jsx
@@ -5,24 +5,25 @@ export const Footer = () => {
     return (
         <footer>
             <div className="footer-body text-dark">
-                <div className="div-footer">
+                <div className="
+                ">
                     <h3 className="title-footer">ÚNETE A NUESTRO DISCORD</h3>
                     <p className="p-footer">Ven al servidor oficial de GG-Hunter y chatea con otros usuarios.</p>
                     <a href="https://discord.gg/G3vKZ7DJXk" className="icon-discord" target="_blank">
-                        <i className="fa-brands fa-discord text-black"></i>
+                        <i className="fa-brands fa-discord"></i>
                     </a>
                 </div>
                 <div className="div-footer">
                     <h3 className="title-footer fs-4">SÍGUENOS</h3>
                     <p className="p-footer">¡Sigue nuestras páginas oficiales para más noticias!</p>
                     <a className="icon-social" href='https://www.facebook.com/profile.php?id=61565137364035' target="_blank">
-                        <i className="fa-brands fa-facebook-f"></i>
+                    <i className="fa-brands fa-facebook"></i>
                     </a>
                     <a className="icon-social" href='https://x.com/home' target="_blank">
                         <i className="fa-brands fa-twitter"></i>
                     </a>
                     <a className="icon-social" href='https://www.instagram.com/_gg_hunter_/' target="_blank">
-                        <i className="fa-brands fa-square-instagram"></i>
+                    <i className="fa-brands fa-instagram"></i>
                     </a>
                 </div>
                 <div className="div-footer">
